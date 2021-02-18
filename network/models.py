54c9ec2213a735b729cac3da3757f381b628c617
chriskorsak <chris@chriskorsak.com>
2021-02-17 21:36:00 -0800

    add user and post classes to models.py

--- network/models.py
+++ network/models.py
@@ -3,4 +3,18 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+  pass
+  followers =
+  following = 
+
+# create classes for posts, likes, and followers. not sure if i need following and followers classes independently.
+
+class Post(models.Model):
+  creator = models.ForeignKey(User, on_delete=models.CASCADE)
+  date = DateTimeField(auto_now_add=True)
+  text = models.CharField(max_length=280)
+  likes = models.IntegerField(default=0)
+
+  def __str__(self):
+        return f"{self.user} {self.date} {self.likes}"
+
